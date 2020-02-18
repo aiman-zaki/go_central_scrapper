41404c617aada85ef7211298fc7800b75a83e424
aiman zaki <aiman0zaki@gmail.com>
2020-02-18 21:51:12 +0800

    init

--- go.mod
+++ go.mod
@@ -1,28 +1,10 @@
-module github.com/aiman-zaki/go_icress_scrapper
+module github.com/aiman-zaki/go_central_scrapper
 
 go 1.13
 
 require (
-	github.com/DataDog/zstd v1.4.4 // indirect
-	github.com/PuerkitoBio/goquery v1.5.0 // indirect
-	github.com/antchfx/htmlquery v1.2.2 // indirect
-	github.com/antchfx/xmlquery v1.2.3 // indirect
-	github.com/antchfx/xpath v1.1.4 // indirect
-	github.com/go-stack/stack v1.8.0 // indirect
-	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/aiman-zaki/go_icress_scrapper v0.0.2
 	github.com/gocolly/colly v1.2.0
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/snappy v0.0.1 // indirect
-	github.com/google/go-cmp v0.4.0 // indirect
-	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
-	github.com/temoto/robotstxt v1.1.1 // indirect
-	github.com/tidwall/pretty v1.0.1 // indirect
-	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
-	github.com/xdg/stringprep v1.0.0 // indirect
 	go.mongodb.org/mongo-driver v1.2.1
-	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	google.golang.org/appengine v1.6.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"reflect"
-	"strings"
-	"time"
 	"unicode"
 
-	"context"
-	"log"
+	scrapper "github.com/aiman-zaki/go_central_scrapper/scrappers"
 
-	models "github.com/aiman-zaki/go_icress_scrapper/models"
-
-	"github.com/gocolly/colly"
+	models "github.com/aiman-zaki/go_central_scrapper/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -42,109 +39,6 @@ func removeSpace(s string) string {
 	return string(rr)
 }
 
-func fetchFaculties() []models.Faculty {
-	var faculties []models.Faculty
-	const URL = "http://icress.uitm.edu.my/jadual/jadual/jadual.asp"
-	c := colly.NewCollector(
-		colly.AllowedDomains("icress.uitm.edu.my"),
-	)
-	c.OnHTML("option", func(e *colly.HTMLElement) {
-		temp := models.Faculty{}
-		tempString := strings.Split(e.Text, "-")
-		temp.Code = tempString[0]
-		temp.Name = tempString[1]
-		faculties = append(faculties, temp)
-		println(faculties)
-	})
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
-	})
-	c.Visit(URL)
-	return faculties
-}
-
-func fetchCourses(f models.Faculty) models.Faculty {
-	var courses []models.Course
-	var URL = "http://icress.uitm.edu.my/jadual/" + f.Code + "/" + f.Code + ".html"
-	c := colly.NewCollector(
-		colly.AllowedDomains("icress.uitm.edu.my"),
-	)
-
-	c.Limit(&colly.LimitRule{
-		DomainGlob:  "*icress.*",
-		Parallelism: 2,
-		RandomDelay: 5 * time.Second,
-	})
-
-	c.OnHTML("a", func(e *colly.HTMLElement) {
-		course := models.Course{}
-		course.Code = e.Text
-		course.Timetables = fetchTimetables(f.Code, course.Code)
-		courses = append(courses, course)
-		f.Courses = courses
-	})
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
-	})
-	c.Visit(URL)
-
-	c.Wait()
-	return f
-
-}
-
-func fetchTimetables(faculty string, course string) []models.Timetable {
-	var URL = "http://icress.uitm.edu.my/jadual/" + faculty + "/" + course + ".html"
-	var timetables []models.Timetable
-
-	c := colly.NewCollector(
-		colly.AllowedDomains("icress.uitm.edu.my"),
-	)
-
-	c.OnHTML("table tbody tr", func(e *colly.HTMLElement) {
-		temp := models.Timetable{}
-		e.ForEach("td", func(index int, el *colly.HTMLElement) {
-			trimpedText := removeSpace(el.Text)
-			switch index {
-			case 0:
-				temp.Group = trimpedText
-			case 1:
-				temp.Start = trimpedText
-			case 2:
-				temp.End = trimpedText
-			case 3:
-				temp.Day = trimpedText
-			case 4:
-				temp.Mode = trimpedText
-			case 5:
-				temp.Status = trimpedText
-			case 6:
-				temp.Room = trimpedText
-			}
-
-		})
-		fmt.Printf("%+v\n", temp)
-		timetables = append(timetables, temp)
-
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
-	})
-	c.Visit(URL)
-	return timetables
-
-}
-
 func main() {
 
 	//MongoDb Connection
@@ -156,11 +50,11 @@ func main() {
 		log.Println("Connected!")
 	}
 
-	faculties := fetchFaculties()
+	faculties := scrapper.FetchFaculties()
 	for index, faculty := range faculties {
 		temp := models.ReadOneFaculty(c, bson.M{"code": faculty.Code})
 		if (reflect.DeepEqual(temp, models.Faculty{})) {
-			faculties[index] = fetchCourses(faculty)
+			faculties[index] = scrapper.FetchCourses(faculty)
 			insertedID := models.InsertNewFaculty(c, faculties[index])
 			log.Println(insertedID)
 		} else {
